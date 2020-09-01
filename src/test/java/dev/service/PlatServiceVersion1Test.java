c7a38fcf137defc5e2a0daa748c1c963b3010c04
Helvin <hjwc86@gmail.com>
2020-09-01 13:20:13 +0200

    TP 3.2 - Test unitaire avec mock

---
+++ src/test/java/dev/service/PlatServiceVersion1Test.java
@@ -0,0 +1,50 @@
+package dev.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import dev.dao.IPlatDao;
+import dev.exception.PlatException;
+
+class PlatServiceVersion1Test {
+
+	private PlatServiceVersion1 service;
+	private IPlatDao dao;
+
+	@BeforeEach
+	public void setUp() {
+		dao = Mockito.mock(IPlatDao.class);
+		service = new PlatServiceVersion1(dao);
+	}
+
+	@Test
+	void ajouterPlatNomInvalideTest() {
+
+		Assertions.assertThrows(PlatException.class, () -> {
+			service.ajouterPlat("riz", 50000);
+			Mockito.verify(dao).ajouterPlat("riz", 50000);
+		});
+
+	}
+
+	@Test
+	void ajouterPlatPrixInvalideTest() {
+
+		Assertions.assertThrows(PlatException.class, () -> {
+			service.ajouterPlat("rizotto", 50);
+			Mockito.verify(dao).ajouterPlat("rizotto", 50);
+		});
+
+	}
+
+	@Test
+	void ajouterPlatValideTest() throws PlatException {
+
+		service.ajouterPlat("rizotto", 501);
+		Mockito.verify(dao).ajouterPlat("rizotto", 501);
+
+	}
+
+}
