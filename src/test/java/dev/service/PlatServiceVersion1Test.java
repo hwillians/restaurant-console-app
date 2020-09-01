f3526c03fd28ce3e198613d003e417d748849e56
Helvin <hjwc86@gmail.com>
2020-09-01 14:05:44 +0200

    to import static

--- src/test/java/dev/service/PlatServiceVersion1Test.java
+++ src/test/java/dev/service/PlatServiceVersion1Test.java
@@ -1,9 +1,9 @@
 package dev.service;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import dev.dao.IPlatDao;
 import dev.exception.PlatException;
 
@@ -14,24 +14,24 @@ class PlatServiceVersion1Test {
 
 	@BeforeEach
 	public void setUp() {
-		dao = Mockito.mock(IPlatDao.class);
+		dao = mock(IPlatDao.class);
 		service = new PlatServiceVersion1(dao);
 	}
 
 	@Test
 	void ajouterPlatNomInvalideTest() {
-		Assertions.assertThrows(PlatException.class, () -> service.ajouterPlat("riz", 501));
+		assertThrows(PlatException.class, () -> service.ajouterPlat("riz", 501));
 	}
 
 	@Test
 	void ajouterPlatPrixInvalideTest() {
-		Assertions.assertThrows(PlatException.class, () -> service.ajouterPlat("rizotto", 500));
+		assertThrows(PlatException.class, () -> service.ajouterPlat("rizotto", 500));
 	}
 
 	@Test
 	void ajouterPlatValideTest() throws PlatException {
 		service.ajouterPlat("rizotto", 501);
-		Mockito.verify(dao).ajouterPlat("rizotto", 501);
+		verify(dao).ajouterPlat("rizotto", 501);
 	}
 
 }
