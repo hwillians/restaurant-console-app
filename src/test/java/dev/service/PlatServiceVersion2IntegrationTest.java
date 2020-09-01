12a3e24705ac476fdababf755d19c8d952b71f21
Helvin <hjwc86@gmail.com>
2020-09-01 14:05:01 +0200

    To import static

---
+++ src/test/java/dev/service/PlatServiceVersion2IntegrationTest.java
@@ -0,0 +1,14 @@
+package dev.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlatServiceVersion2IntegrationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
