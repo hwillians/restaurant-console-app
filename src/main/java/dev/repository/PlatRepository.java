efe6445afd8378c5b3beb86b89351a0dc506e986
Helvin <hjwc86@gmail.com>
2020-09-03 22:35:44 +0200

    TP 4.4 - Spring Data - Tests d’intégration PlatRepositoryIntegrationTest

--- src/main/java/dev/repository/PlatRepository.java
+++ src/main/java/dev/repository/PlatRepository.java
@@ -1,13 +1,27 @@
 package dev.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dev.entite.Plat;
 
 public interface PlatRepository extends JpaRepository<Plat, Integer> {
 
 	List<Plat> findByPrixEnCentimesEuros(Integer prixEnCentimesEuros);
+
+	@Query("select avg(p.prixEnCentimesEuros) from Plat p where p.prixEnCentimesEuros > :prix")
+	Double findByAvgPrixSuppA(@Param("prix") int i);
+
+	@Query("select p from Plat p join fetch p.ingredients where p.nom = ?1")
+	Optional<Plat> findByNomWithIngredients(String string);
+
+	@Modifying
+	@Query("update Plat p set p.nom = :newNom WHERE p.nom = :oldNom")
+	void updateNomPlat(@Param("oldNom") String ancienNom, @Param("newNom") String nouveauNom);
 
 }
--- src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
+++ src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
@@ -2,6 +2,7 @@ package dev.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +19,7 @@ import dev.config.JdbcTestConfig;
 import dev.config.JpaConfig;
 import dev.dao.BasePlatDaoBddIntegrationTest;
 import dev.dao.PlatDaoJpa;
+import dev.entite.Ingredient;
 import dev.entite.Plat;
 
 @SpringJUnitConfig(classes = { PlatDaoJpa.class, JdbcTestConfig.class, JpaConfig.class })
@@ -29,7 +31,6 @@ public class PlatRepositoryIntegrationTest extends BasePlatDaoBddIntegrationTest
 
 	@Test
 	void testFindAll() {
-
 		List<Plat> plats = platRepository.findAll();
 		assertThat(plats).isNotEmpty();
 	}
@@ -37,13 +38,13 @@ public class PlatRepositoryIntegrationTest extends BasePlatDaoBddIntegrationTest
 	@Test
 	void testFindAllSortAsc() {
 		List<Plat> plats = platRepository.findAll(Sort.by("prixEnCentimesEuros").ascending());
-		assertThat(plats.get(0).getPrixEnCentimesEuros()).isLessThan(plats.get(1).getPrixEnCentimesEuros());
+		assertThat(plats).isSortedAccordingTo(Comparator.comparing(Plat::getPrixEnCentimesEuros));
 	}
 
 	@Test
 	void estFindAllSortDesc() {
 		List<Plat> plats = platRepository.findAll(Sort.by("prixEnCentimesEuros").descending());
-		assertThat(plats.get(0).getPrixEnCentimesEuros()).isGreaterThan(plats.get(1).getPrixEnCentimesEuros());
+		assertThat(plats).isSortedAccordingTo(Comparator.comparing(Plat::getPrixEnCentimesEuros).reversed());
 	}
 
 	@Test
@@ -56,8 +57,8 @@ public class PlatRepositoryIntegrationTest extends BasePlatDaoBddIntegrationTest
 	@Test
 	void testFindById() {
 		Optional<Plat> p = platRepository.findById(3);
-		assertThat(p.get().getNom()).isEqualTo("Couscous");
-
+		Plat plat = p.orElseThrow(() -> new RuntimeException("L'id = 4 existe, la requête devrait retourner un plat"));
+		assertThat(plat.getNom()).isEqualTo("Couscous");
 	}
 
 	@Test
@@ -65,7 +66,6 @@ public class PlatRepositoryIntegrationTest extends BasePlatDaoBddIntegrationTest
 	void testGetOne() {
 		Plat p = platRepository.getOne(3);
 		assertThat(p.getNom()).isEqualTo("Couscous");
-
 	}
 
 	@Test
@@ -75,17 +75,56 @@ public class PlatRepositoryIntegrationTest extends BasePlatDaoBddIntegrationTest
 
 	@Test
 	void testFindByPrix() {
-
-		List<Plat> plats = platRepository.findByPrixEnCentimesEuros(1300);
-
 		assertThat(platRepository.findByPrixEnCentimesEuros(1300)).extracting(Plat::getNom)
 				.contains("Magret de canard");
-
 	}
 
 	@Test
 	void testAvgPrix() {
+		Double moyenne = platRepository.findByAvgPrixSuppA(1000);
+		double total = 0;
+		int compte = 0;
+		List<Plat> plats = platRepository.findAll();
+		for (Plat p : plats) {
+			if (p.getPrixEnCentimesEuros() > 1000) {
+				total += p.getPrixEnCentimesEuros();
+				compte++;
+			}
+		}
+		assertThat(moyenne).isEqualTo(total / compte);
 
 	}
 
+	@Test
+	void testFindByNomWithIngredients() {
+		Optional<Plat> p = platRepository.findByNomWithIngredients("Moules-frites");
+		Plat plat = p.orElseThrow(() -> new RuntimeException("Magret de canard existe !!!"));
+		assertThat(plat.getIngredients()).extracting(Ingredient::getNom).contains("Moule", "Beurre", "Persil", "Thym",
+				"Poivre", "Vin blanc sec");
+	}
+
+	@Test
+	void testSave() {
+
+		long countBefore = platRepository.count();
+
+		Plat plat = new Plat();
+		plat.setNom("pollo");
+		plat.setPrixEnCentimesEuros(2500);
+		platRepository.save(plat);
+
+		long countAfter = platRepository.count();
+		assertThat(countAfter).isEqualTo(countBefore + 1);
+		assertThat(platRepository.findAll()).extracting(Plat::getNom).contains(plat.getNom());
+		assertThat(platRepository.findAll()).extracting(Plat::getPrixEnCentimesEuros)
+				.contains(plat.getPrixEnCentimesEuros());
+	}
+
+	@Test
+	@Transactional
+	void testChangerNom() {
+		platRepository.updateNomPlat("Couscous", "Couscous-Merguez");
+		List<Plat> listPlat = platRepository.findAll();
+		assertThat(listPlat).extracting(Plat::getNom).contains("Couscous-Merguez");
+	}
 }
